da5250de0d92976071cf35b9050b1cf17efb084b
David Brownell <db@DavidBrownell.com>
2015-11-18 18:12:49 -0800

    Added pss v1.40

---
+++ Libraries/Python/psslib/v1.40/__scripts__/Windows/pss-script.py
@@ -0,0 +1,10 @@
+#!C:\Code\v2\Common\Environment\Generated\Default\Python\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pss==1.40','console_scripts','pss'
+__requires__ = 'pss==1.40'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pss==1.40', 'console_scripts', 'pss')()
+    )
