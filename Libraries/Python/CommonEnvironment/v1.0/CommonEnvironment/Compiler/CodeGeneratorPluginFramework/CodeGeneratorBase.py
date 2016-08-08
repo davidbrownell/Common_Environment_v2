b5cb3119b0587cfa62d1377940c8ee1fdac611f4
David Brownell <db@DavidBrownell.com>
2016-08-08 05:58:08 -0700

    Added CodeGeneratorPluginFramework for code generators that are implemented in terms of dyanmic plugins.

---
+++ Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/Compiler/CodeGeneratorPluginFramework/CodeGeneratorBase.py
@@ -0,0 +1,366 @@
+# ----------------------------------------------------------------------
+# |  
+# |  CodeGeneratorBase.py
+# |  
+# |  David Brownell <db@DavidBrownell.com>
+# |      2016-08-04 13:31:21
+# |  
+# ----------------------------------------------------------------------
+# |  
+# |  Copyright David Brownell 2016.
+# |  Distributed under the Boost Software License, Version 1.0.
+# |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# |  
+# ----------------------------------------------------------------------
+import imp
+import os
+import sys
+
+from collections import OrderedDict
+
+from CommonEnvironment.CallOnExit import CallOnExit
+from CommonEnvironment import CommandLine
+from CommonEnvironment import FileSystem
+from CommonEnvironment.Interface import *
+from CommonEnvironment.NamedTuple import NamedTuple
+from CommonEnvironment.TypeInfo.FundamentalTypes import *
+from CommonEnvironment.StreamDecorator import StreamDecorator
+
+from CommonEnvironment.Compiler import CodeGenerator as CodeGeneratorMod
+
+from CommonEnvironment.Compiler.InputProcessingMixin.AtomicInputProcessingMixin import AtomicInputProcessingMixin
+from CommonEnvironment.Compiler.InvocationQueryMixin.ConditionalInvocationQueryMixin import ConditionalInvocationQueryMixin
+from CommonEnvironment.Compiler.InvocationMixin.CustomInvocationMixin import CustomInvocationMixin
+from CommonEnvironment.Compiler.OutputMixin.MultipleOutputMixin import MultipleOutputMixin
+
+# ----------------------------------------------------------------------
+_script_fullpath = os.path.abspath(__file__) if "python" in sys.executable.lower() else sys.executable
+_script_dir, _script_name = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+PluginInfo = NamedTuple("PluginInfo", "mod", "plugin")
+
+# ----------------------------------------------------------------------
+def GetPlugins( dynamic_plugin_architecture_environment_key,
+                plugins_dir,
+              ):
+    # There are 2 ways of loading plugins:
+    #   1) Using the dynamic plugin architecture
+    #   2) Files found in a specific directory
+
+    plugin_mods = []
+
+    if os.getenv(dynamic_plugin_architecture_environment_key):
+        assert os.getenv("DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL")
+
+        sys.path.insert(0, os.getenv("DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL"))
+        with CallOnExit(lambda: sys.path.pop(0)):
+            from SourceRepositoryTools import DynamicPluginArchitecture
+
+            plugin_mods = list(DynamicPluginArchitecture.EnumeratePlugins(dynamic_plugin_architecture_environment_key))
+
+    else:
+        if not os.path.isdir(plugins_dir):
+            raise Exception("The directory '{]' does not exist".format(plugins_dir))
+
+        for filename in FileSystem.WalkFiles( plugins_dir,
+                                              include_file_base_names=[ lambda name: name.endswith("Plugin"), ],
+                                              include_file_extensions=[ ".py", ],
+                                              recurse=True,
+                                            ):
+            name = os.path.splitext(os.path.basename(filename))[0]
+            plugin_mods.append(imp.load_source(name, filename))
+
+    plugins = OrderedDict()
+
+    # ----------------------------------------------------------------------
+    # This method will be added to all plugins discovered
+    def GetPluginsImpl(plugin_name):
+        if plugin_name not in plugins:
+            raise Exception("'{}' is not a valid plugin".format(plugin_name))
+
+        return plugins[plugin_name]
+
+    # ----------------------------------------------------------------------
+    
+    warning_stream = StreamDecorator( sys.stdout,
+                                      line_prefix="WARNING: ",
+                                      suffix='\n',
+                                    )
+    with CallOnExit(lambda: warning_stream.flush(force_suffix=True)):
+        if not plugin_mods:
+            warning_stream.write("No plugins were found.\n")
+
+        for plugin_mod in plugin_mods:
+            obj = getattr(plugin_mod, "Plugin", None)
+            if obj == None:
+                warning_stream.write("The module defined at '{}' does not contain a 'Plugin' class.\n".format(plugin_mod.__file__))
+                continue
+
+            # Dynamically add the method GetPlugin to the plugin objects; this will
+            # allow a plugin to query for other plugins. This is a bit wonky, but it
+            # works with the plugin architecture where most of the plugins are static
+            # objects.
+            obj.GetPlugin = staticmethod(GetPluginsImpl)
+
+            obj = obj()
+
+            if not obj.IsValidEnvironment():
+                warning_stream.write("The plugin '{}' is not valid within this environment ({}).\n".format(obj.Name, plugin_mod.__file__))
+                continue
+
+            plugins[obj.Name] = PluginInfo(plugin_mod, obj)
+
+    return plugins
+
+# ----------------------------------------------------------------------
+def CodeGeneratorFactory( plugin_info_map,
+                          name,
+                          description,
+                          is_supported_func,            # def Func(item) -> bool
+                          get_optional_metadata_func,   # def Func() -> [ (k, v), ...]
+                          preprocess_context_func,      # def Func(context) -> context
+                          postprocess_context_func,     # def Func(context) -> context
+                          invoke_func,                  # def Func(cls, invoke_reason, context, status_stream, output_stream, plugin)
+                        ):
+    assert is_supported_func
+    assert get_optional_metadata_func
+    assert preprocess_context_func
+    assert postprocess_context_func
+
+    # ----------------------------------------------------------------------
+    @staticderived
+    class CodeGenerator( AtomicInputProcessingMixin,
+                         ConditionalInvocationQueryMixin,
+                         CustomInvocationMixin,
+                         MultipleOutputMixin,
+                         CodeGeneratorMod.CodeGenerator,
+                       ):
+        # ----------------------------------------------------------------------
+        # |  
+        # |  Public Properties
+        # |  
+        # ----------------------------------------------------------------------
+        Name                                = name
+        Description                         = description
+        Type                                = CodeGeneratorMod.CodeGenerator.TypeValue.File
+
+        # ----------------------------------------------------------------------
+        # |  
+        # |  Public Methods
+        # |  
+        # ----------------------------------------------------------------------
+        @staticmethod
+        def IsSupported(item):
+            return is_supported_func(item)
+
+        # ----------------------------------------------------------------------
+        # |  
+        # |  Protected Methods
+        # |  
+        # ----------------------------------------------------------------------
+        @classmethod
+        def _GetAdditionalGeneratorItems(cls, context):
+            # ----------------------------------------------------------------------
+            def ProcessGeneratorItem(item):
+                if isinstance(item, (str, unicode)):
+                    assert item in plugin_info_map, item
+                    return plugin_info_map[item].plugin
+
+                return item
+
+            # ----------------------------------------------------------------------
+            
+            plugin = plugin_info_map[context.plugin_name].plugin
+
+            return [ cls, plugin, ] + \
+                   [ ProcessGeneratorItem(item) for item in plugin.GetAdditionalGeneratorItems(context) ] + \
+                   super(CodeGenerator, cls)._GetAdditionalGeneratorItems(context)
+
+        # ----------------------------------------------------------------------
+        # |  
+        # |  Private Methods
+        # |  
+        # ----------------------------------------------------------------------
+        @classmethod
+        def _GetRequiredMetadataNames(cls):
+            return [ "plugin_name",
+                     "output_name",
+                   ] + \
+                   super(CodeGenerator, cls)._GetRequiredMetadataNames()
+
+        # ----------------------------------------------------------------------
+        @classmethod
+        def _GetOptionalMetadata(cls):
+            return get_optional_metadata_func() + super(CodeGenerator, cls)._GetOptionalMetadata()
+
+        # ----------------------------------------------------------------------
+        @classmethod
+        def _PostprocessContextItem(cls, context):
+            if context.plugin_name not in plugin_info_map:
+                raise CommandLine.UsageException("'{}' is not a valid plugin".format(context.plugin_name))
+
+            plugin = plugin_info_map[context.plugin_name].plugin
+
+            # Ensure that all plugin settings are present and that they
+            # are the expected type.
+            custom_settings = { k : v for k. v in plugin.GenerateCustomSettingsAndDefaults() }
+
+            for k, v in context.plugin_settings.iteritems():
+                if k not in custom_settings:
+                    raise CommandLine.UsageException("'{}' is not a valid key for plugin_arg".format(k))
+
+                desired_type = type(custom_settings[k])
+
+                if type(v) != desired_type:
+                    assert isinstance(v, (str, unicode))
+                    context.plugin_settings[k] = FundamentalTypeInfo.CreateTypeInfo(desired_type).ItemFromString(v)
+
+            for k, v in custom_settings.iteritems():
+                if k not in context.plugin_settings:
+                    context.plugin_settings[k] = v
+
+            context.plugin_settings = plugin.PreprocessCustomSettings(**context.plugin_settings)
+
+            # Invoke custom functionality
+            context = preprocess_context_func(context)
+
+            # Postprocess and validate the context with the plugin
+            context = plugin.PostprocessContextItem(context, pre_validate=True)
+
+            context.output_filenames = [ os.path.join(context.output_dir, filename) for filename in plugin.GenerateOutputFilenames(context) ]
+
+            plugin.ValidateContextItem(context)
+
+            context = plugin.PostprocessContextItem(context, pre_validate=False)
+
+            context = postprocess_context_func(context)
+
+            return super(CodeGenerator, cls)._PostprocessContextItem(context)
+
+        # ----------------------------------------------------------------------
+        @classmethod
+        def _InvokeImpl( cls,
+                         invoke_reason,
+                         context,
+                         status_stream,
+                         output_stream,
+                       ):
+            return invoke_func( cls,
+                                invoke_reason,
+                                context, 
+                                status_stream,
+                                output_stream,
+                                plugin_info_map[context.plugin_name].plugin,
+                              )
+
+    # ----------------------------------------------------------------------
+    
+    return CodeGenerator
+
+# ----------------------------------------------------------------------
+def GenerateFactory( plugin_info_map,
+                     code_generator,
+                   ):
+    # ----------------------------------------------------------------------
+    @CommandLine.EntryPoint()
+    @CommandLine.FunctionConstraint( plugin=CommandLine.EnumTypeInfo(plugin_info_map.keys()),
+                                     output_name=CommandLine.StringTypeInfo(),
+                                     output_dir=CommandLine.DirectoryTypeInfo(ensure_exists=False),
+                                     input=CommandLine.FilenameTypeInfo(CommandLine.FilenameTypeInfo.Type_Either, arity='+'),
+                                     plugin_arg=CommandLine.DictTypeInfo(require_exact_match=False),
+                                     output_stream=None,
+                                   )
+    def Generate( plugin,
+                  output_name,
+                  output_dir,
+                  input,
+                  plugin_arg={},
+                  force=False,
+                  output_stream=sys.stdout,
+                  verbose=False,
+                ):
+        return CodeGeneratorMod.CommandLineGenerate( code_generator,
+                                                     input,
+                                                     output_stream,
+                                                     verbose,
+
+                                                     plugin_name=plugin,
+                                                     output_name=output_name,
+                                                     output_dir=output_dir,
+
+                                                     plugin_settings=plugin_arg,
+
+                                                     force=force,
+                                                   )
+
+    # ----------------------------------------------------------------------
+    
+    return Generate
+
+# ----------------------------------------------------------------------
+def CleanFactory():
+    # ----------------------------------------------------------------------
+    @CommandLine.EntryPoint()
+    @CommandLine.FunctionConstraints( output_dir=CommandLine.DirectoryTypeInfo(),
+                                      output_stream=None,
+                                    )
+    def Clean( output_dir,
+               output_stream=sys.stdout,
+             ):
+        return CodeGeneratorMod.CommandLineClean(output_dir, output_stream)
+
+    # ----------------------------------------------------------------------
+    
+    return Clean
+
+# ----------------------------------------------------------------------
+def ListFactory(plugin_info_map):
+    # ----------------------------------------------------------------------
+    @CommandLine.EntryPoint()
+    @CommandLine.FunctionConstraints( output_stream=None,
+                                    )
+    def List( json=False,
+              output_stream=sys.stdout,
+            ):
+        if json:
+            d = OrderedDict()
+
+            # ----------------------------------------------------------------------
+            def ProcessEntry(k, v):
+                mod_filename = v.mod.__file__
+    
+                if os.path.splitext(mod_filename)[1] == ".pyc":
+                    mod_filename = mod_filename[:-1]
+    
+                assert os.path.isfile(mod_filename), mod_filename
+    
+                d[k] = { "python_filename" : mod_filename,
+                       }
+    
+            # ----------------------------------------------------------------------
+            def OnComplete():
+                import json as json_mod
+                output_stream.write(json_mod.dumps(d))
+    
+            # ----------------------------------------------------------------------
+            
+        else:
+            # ----------------------------------------------------------------------
+            def ProcessEntry(k, v):
+                output_stream.write("{}\n".format(k))
+    
+            # ----------------------------------------------------------------------
+            def OnComplete():
+                pass
+    
+            # ----------------------------------------------------------------------
+            
+        with CallOnExit(OnComplete):
+            for k, v in plugin_info_map.iteritems():
+                ProcessEntry(k, v.plugin)
+
+    # ----------------------------------------------------------------------
+    
+    return List
