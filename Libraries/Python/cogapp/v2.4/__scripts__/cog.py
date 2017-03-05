c6d2675e2cc91b9bc2f6be592cf3a2239ad504da
David Brownell <db@DavidBrownell.com>
2017-03-05 10:10:34 -0800

    Dynamically loading eggs

--- Libraries/Python/cogapp/v2.4/__scripts__/cog.py
+++ Libraries/Python/cogapp/v2.4/__scripts__/cog.py
@@ -1,4 +1,4 @@
-#!C:\Code\v2\Common\Environment\Generated\Default\Python\python.exe
+#/usr/bin/env python
 # EASY-INSTALL-SCRIPT: 'cogapp==2.4','cog.py'
 __requires__ = 'cogapp==2.4'
 __import__('pkg_resources').run_script('cogapp==2.4', 'cog.py')
