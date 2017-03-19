9c137f2dbee3e9b2a8c7c9136fc6ca67ab24ebdd
David Brownell <db@DavidBrownell.com>
2017-03-19 15:50:22 -0700

    Added pypiwin32
    --HG--
    branch : Python 3.6

---
+++ Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/Impl/Python2Helpers.py
@@ -0,0 +1,29 @@
+# ----------------------------------------------------------------------
+# |  
+# |  Python2Helpers.py
+# |  
+# |  David Brownell <db@DavidBrownell.com>
+# |      2017-03-19 14:19:36
+# |  
+# ----------------------------------------------------------------------
+# |  
+# |  Copyright David Brownell 2017.
+# |  Distributed under the Boost Software License, Version 1.0.
+# |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# |  
+# ----------------------------------------------------------------------
+"""Helpers for Python2 code"""
+
+import os
+import sys
+
+# ----------------------------------------------------------------------
+_script_fullpath = os.path.abspath(__file__) if "python" in sys.executable.lower() else sys.executable
+_script_dir, _script_name = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# The 'ur' syntax can't be parsed in Python3, so only include this file
+# if we are in Python2.
+def ConvertUNCFilename(filename):
+    return ur"\\?\{}".format(filename)
