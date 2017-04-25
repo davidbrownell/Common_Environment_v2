bbe7958e8bb15b0b85dc0c2aafbd61784f02d948
David Brownell <db@DavidBrownell.com>
2017-04-25 13:43:52 -0700

    Enhancements for six_plus

---
+++ Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/Impl/six_plus/v2.py
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------
+# |  
+# |  v2.py
+# |  
+# |  David Brownell <db@DavidBrownell.com>
+# |      2017-04-25 08:07:31
+# |  
+# ----------------------------------------------------------------------
+# |  
+# |  Copyright David Brownell 2017.
+# |  Distributed under the Boost Software License, Version 1.0.
+# |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# |  
+# ----------------------------------------------------------------------
+import os
+import sys
+
+# ----------------------------------------------------------------------
+_script_fullpath = os.path.abspath(__file__) if "python" in sys.executable.lower() else sys.executable
+_script_dir, _script_name = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+def CreateMetaClass(base_class, meta_class):
+    # ----------------------------------------------------------------------
+    class MetaClass(base_class):
+        __metaclass__                       = meta_class
+
+    # ----------------------------------------------------------------------
+    
+    return MetaClass
