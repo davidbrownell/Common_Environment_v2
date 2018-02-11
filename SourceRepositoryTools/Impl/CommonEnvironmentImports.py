57edf45ebef1e368a7e376a609ebe8103086787f
David Brownell <db@DavidBrownell.com>
2018-02-10 22:19:23 -0800

    Added functionality to import core libraries forcefully if they don't exist

--- SourceRepositoryTools/Impl/CommonEnvironmentImports.py
+++ SourceRepositoryTools/Impl/CommonEnvironmentImports.py
@@ -31,6 +31,38 @@ import Constants
 del sys.path[0]
 
 # ----------------------------------------------------------------------
+# This code will be called extremely early in the activation cycle, and sometimes
+# outside of the Tools dir (as is the case when executed as a Mercurial plugin,
+# which uses an embedded version of Python). There are some modules (like six) that
+# are absoluately critical to everything in CommonEnvironment and need to be loaded
+# in all scenarios. As a fail safe, hard code a path for some of these libraries if
+# they aren't otherwise available.
+try:
+    import six
+except ImportError:
+    # At this point, use a python dir that represents the lowest common denominator.
+    # These libraries are so fundamental that they won't be doing anything OS-
+    # specific, so any path will do.
+    python_root = os.path.realpath(os.path.join(_script_dir, "..", "..", "Tools", "Python", "v2.7.10"))
+    assert os.path.isdir(python_root), python_root
+
+    for potential_suffix in [ os.path.join("Windows", "Lib", "site-packages"),
+                              os.path.join("Ubuntu", "lib", "python2.7", "site-packages"),
+                            ]:
+        potential_path = os.path.join(python_root, potential_suffix)
+        if os.path.isdir(potential_path):
+            sys.path.insert(0, potential_path)
+            break
+
+    # Try it agian
+    import inflect
+    import semantic_version
+    import six
+    import wrapt
+
+    del sys.path[0]
+
+# ----------------------------------------------------------------------
 # |  COMMON_ENVIRONMENT_PATH
 COMMON_ENVIRONMENT_PATH                     = os.path.join(Constants.DE_FUNDAMENTAL, "Libraries", "Python", "CommonEnvironment", "v1.0")
 assert os.path.isdir(COMMON_ENVIRONMENT_PATH), COMMON_ENVIRONMENT_PATH
